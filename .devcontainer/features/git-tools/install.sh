ba99725877727e4966c01ed7c4a6c41a4373a1db
Konstantin A. Olkhovskiy <lupus@oxnull.net>
2025-05-19 08:33:31 +0000

    Remove whole-script priviledge drop for git-tools feature

--- .devcontainer/features/git-tools/install.sh
+++ .devcontainer/features/git-tools/install.sh
@@ -10,13 +10,6 @@ if [ -z "${EXISTING_NON_ROOT_USER:-}" ]; then
   EXISTING_NON_ROOT_USER="root"
 fi
 
-if [ "${1:-}" != "--as-user" ]; then
-  # Re-run the script as the target user, passing --as-user to avoid recursion
-  SCRIPT=$(readlink -f "$0")
-  PWD=$(pwd)
-  exec su - "$EXISTING_NON_ROOT_USER" -c "bash -c \"cd $PWD && $SCRIPT --as-user\""
-fi
-
 # Install git-split
 echo "(*) Installing git-split..."
 (
