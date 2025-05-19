a7e9cb3f41ff63c0358cc74ee6456771ea370622
Lupus <Lupus@users.noreply.github.com>
2025-05-19 20:09:11 +0000

    deploy: e385e14c9d791c19dc05b7202e2ee9a1232c8460

--- trait.impl/core/convert/trait.AsMut.js
+++ trait.impl/core/convert/trait.AsMut.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["ocaml_rs_smartptr",[["impl&lt;P: <a class=\"trait\" href=\"ocaml_rs_smartptr/ocaml_gen_extras/trait.TypeParams.html\" title=\"trait ocaml_rs_smartptr::ocaml_gen_extras::TypeParams\">TypeParams</a>, T: FromValue + ToValue&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.86.0/core/convert/trait.AsMut.html\" title=\"trait core::convert::AsMut\">AsMut</a>&lt;T&gt; for <a class=\"struct\" href=\"ocaml_rs_smartptr/ocaml_gen_extras/struct.WithTypeParams.html\" title=\"struct ocaml_rs_smartptr::ocaml_gen_extras::WithTypeParams\">WithTypeParams</a>&lt;P, T&gt;"]]]]);
+    var implementors = Object.fromEntries([["ocaml_rs_smartptr",[["impl&lt;P: <a class=\"trait\" href=\"ocaml_rs_smartptr/ocaml_gen_extras/trait.TypeParams.html\" title=\"trait ocaml_rs_smartptr::ocaml_gen_extras::TypeParams\">TypeParams</a>, T: FromValue + ToValue&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.87.0/core/convert/trait.AsMut.html\" title=\"trait core::convert::AsMut\">AsMut</a>&lt;T&gt; for <a class=\"struct\" href=\"ocaml_rs_smartptr/ocaml_gen_extras/struct.WithTypeParams.html\" title=\"struct ocaml_rs_smartptr::ocaml_gen_extras::WithTypeParams\">WithTypeParams</a>&lt;P, T&gt;"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
--- trait.impl/core/fmt/trait.Display.js
+++ trait.impl/core/fmt/trait.Display.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["ocaml_rs_smartptr",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.86.0/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"struct\" href=\"ocaml_rs_smartptr/ml_box/struct.MlBox.html\" title=\"struct ocaml_rs_smartptr::ml_box::MlBox\">MlBox</a>"]]]]);
+    var implementors = Object.fromEntries([["ocaml_rs_smartptr",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.87.0/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"struct\" href=\"ocaml_rs_smartptr/ml_box/struct.MlBox.html\" title=\"struct ocaml_rs_smartptr::ml_box::MlBox\">MlBox</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
